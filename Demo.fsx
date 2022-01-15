14fd32a10c3ba19bb572e48bbaf07834b93e7c31
Jakob Christensen <jakob.christensen@gmail.com>
2022-01-15 03:42:40 +0100

    Update demo file

--- Demo.fsx
+++ Demo.fsx
@@ -1,11 +1,12 @@
 #load @"Split.fsx"
 
-// Demo
-{
-    Split.Included = [ 1; 2; 3; 10; 354; 234; 23; 45 ];
-    Split.Excluded = Seq.empty<int>;
-}
-|> Split.split (fun i -> (i % 2) = 0)
-|> Split.iter (fun i -> printfn "%d" i)
-|> Split.clearAndMap (fun i -> i * i)
-|> Split.iter (fun i -> printfn "%d" i)
+let printer title l =
+    printfn "%s: %A" title l
+
+[ 1; 2; 3; 10; 354; 234; 23; 45 ]
+|> Split.create (fun x -> x >= 10)
+|> Split.outputExcluded (printer "Less than 10")
+|> Split.recreate (fun x -> (x % 2) = 0)
+|> Split.outputExcluded (printer "Odd")
+|> Split.switch
+|> Split.outputExcluded (printer "Even")
--- Split.fsx
+++ Split.fsx
@@ -1,77 +1,85 @@
-module Split =
-    type Split<'a> =
-        { Included: 'a list
-          Excluded: 'a list }
+type Split<'a> =
+    { Included: 'a list
+      Excluded: 'a list }
 
-    /// Split a list into two lists, one containing the included items, and the other containing the excluded items.
-    let create predicate (l: 'a list) =
-        let rec doSplit (split: Split<'a>) (l: 'a list) =
-            match l with
-            | [] -> split
-            | x :: xs ->
-                if x |> predicate then
-                    doSplit { split with Included = [x] |> List.append split.Included } xs
-                else
-                    doSplit { split with Excluded = [x] |> List.append split.Excluded } xs
-        
-        doSplit { Included = []; Excluded = [] } l
+/// Split a list into two lists, one containing the included items, and the other containing the excluded items.
+let create predicate (l: 'a list) =
+    let rec doSplit (split: Split<'a>) (l: 'a list) =
+        match l with
+        | [] -> split
+        | x :: xs ->
+            if x |> predicate then
+                doSplit { split with Included = [x] |> List.append split.Included } xs
+            else
+                doSplit { split with Excluded = [x] |> List.append split.Excluded } xs
+    
+    doSplit { Included = []; Excluded = [] } l
 
-    let ofList list =
-        { Included = list
-          Excluded = [] }    
+let ofList list =
+    { Included = list
+      Excluded = [] }    
 
-    let recreate predicate split =
-        split.Included |> create predicate
+let recreate predicate split =
+    split.Included |> create predicate
 
-    let decompose split =
-        split.Included
+let decompose split =
+    split.Included
 
-    let switch split =
-        { Included = split.Excluded
-          Excluded = split.Included }
+let switch split =
+    { Included = split.Excluded
+      Excluded = split.Included }
 
-    let merge split =
-        { Included = split.Excluded |> List.append split.Included
-          Excluded = [] }
+let merge split =
+    { Included = split.Excluded |> List.append split.Included
+      Excluded = [] }
 
-    let clear split =
-        { split with Excluded = [] }
+let clear split =
+    { split with Excluded = [] }
 
-    let map mapping split =
-        { Included = split.Included |> List.map mapping
-          Excluded = split.Excluded |> List.map mapping }
+let map mapping split =
+    { Included = split.Included |> List.map mapping
+      Excluded = split.Excluded |> List.map mapping }
 
-    let choose chooser split =
-        { Included = split.Included |> List.choose chooser
-          Excluded = split.Excluded |> List.choose chooser }
+let choose chooser split =
+    { Included = split.Included |> List.choose chooser
+      Excluded = split.Excluded |> List.choose chooser }
 
-    let filter predicate split =
-        { Included = split.Included |> List.filter predicate
-          Excluded = split.Excluded |> List.filter predicate }
+let filter predicate split =
+    { Included = split.Included |> List.filter predicate
+      Excluded = split.Excluded |> List.filter predicate }
 
-    let sortBy projection split =
-        { Included = split.Included |> List.sortBy projection
-          Excluded = split.Excluded |> List.sortBy projection }
+let sortBy projection split =
+    { Included = split.Included |> List.sortBy projection
+      Excluded = split.Excluded |> List.sortBy projection }
 
-    let sortByDescending projection split =
-        { Included = split.Included |> List.sortByDescending projection
-          Excluded = split.Excluded |> List.sortByDescending projection }
+let sortByDescending projection split =
+    { Included = split.Included |> List.sortByDescending projection
+      Excluded = split.Excluded |> List.sortByDescending projection }
 
-    let collect mapping split =
-        { Included = split.Included |> List.collect mapping
-          Excluded = split.Excluded |> List.collect mapping }
+let collect mapping split =
+    { Included = split.Included |> List.collect mapping
+      Excluded = split.Excluded |> List.collect mapping }
 
-    let outputIncluded printer split =
-        split.Included
-        |> printer
+let outputIncluded printer split =
+    split.Included
+    |> printer
 
-        split
+    split
 
-    let outputExcluded printer split =
-        split.Excluded
-        |> printer
+let outputExcluded printer split =
+    split.Excluded
+    |> printer
 
-        split
+    split
 
-    let outputIncludedAndClear printer =
-        (outputIncluded printer) >> clear
+let outputIncludedAndClear printer =
+    (outputIncluded printer) >> clear
+
+let splitAndOutput predicate printer =
+    create predicate
+    >> outputIncluded printer
+
+let splitAndOutputAndClear predicate printer =
+    create predicate
+    >> outputIncluded printer
+    >> clear
